ab24fa1581de9cb5ea7b35d518e7d5968603c1b5
Ben Griffith <bengriffith@outlook.com>
2026-01-20 20:43:35 -0700

    implementing write to minio

--- dags/include/scripts/fetch_bts_data.py
+++ dags/include/scripts/fetch_bts_data.py
@@ -1,16 +1,19 @@
 import argparse
-import os
-import time
 import datetime
+import time
 from urllib.parse import urljoin
-from http import HTTPStatus
 
 import requests
-
-
-from constants import BTS_START_YEAR, BTS_BASE_URL, BTS_FILENAME, MAX_MONTH
-
-#full_url = urljoin()
+from minio import Minio
+from utils.constants import (
+    BTS_BASE_URL,
+    BTS_FILENAME,
+    BTS_START_YEAR,
+    BUCKET_BRONZE,
+    MAX_MONTH,
+    MINIO_ROOT_PASSWORD,
+    MINIO_ROOT_USER,
+)
 
 # argparse to accept arguments
 # check for valid year
@@ -23,8 +26,21 @@ from constants import BTS_START_YEAR, BTS_BASE_URL, BTS_FILENAME, MAX_MONTH
 # default scenario for optional args
 
 
+def _minio_client():
+    client = Minio(
+        endpoint="localhost:9000",
+        access_key=MINIO_ROOT_USER,
+        secret_key=MINIO_ROOT_PASSWORD,
+        secure=False,
+    )
+    return client
 
-parser = argparse.ArgumentParser(description="Fetch Bureau of Transportation Statistics data")
+
+minio_client = _minio_client()
+
+parser = argparse.ArgumentParser(
+    description="Fetch Bureau of Transportation Statistics data"
+)
 parser.add_argument("--year", type=int, default=datetime.date.today().year)
 parser.add_argument("--month", type=int, default=datetime.date.today().month)
 
@@ -40,20 +56,24 @@ year = args.year
 
 while year <= args.year:
     for i in range(args.month, MAX_MONTH):
-        filename = f"{BTS_FILENAME}_{year}_{args.month}.zip"
-        full_url = urljoin(BTS_BASE_URL, filename)
-        breakpoint()
+        object = f"{BTS_FILENAME}_{year}_{args.month}.zip"
+        full_url = urljoin(BTS_BASE_URL, object)
 
-        directory = os.getcwd()
-        file_path = os.path.join(directory, filename)
         with requests.get(full_url, stream=True) as response:
-            if response.status_code == HTTPStatus.OK:
-                with open(file_path, "wb") as file:
-                    for chunk in response.iter_content(chunk_size=8192):
-                        file.write(chunk)
+            response.raise_for_status()
+
+            file_size = int(response.headers.get("Content-length", 0))
+
+            if file_size > 0:
+                minio_client.put_object(
+                    bucket_name=BUCKET_BRONZE,
+                    object_name=object,
+                    data=response.raw,
+                    length=file_size,
+                    part_size=10 * 1024 * 1024,
+                    content_type=response.headers.get("Content-Type"),
+                )
 
         time.sleep(5)
+
     year += 1
-
-
-    
