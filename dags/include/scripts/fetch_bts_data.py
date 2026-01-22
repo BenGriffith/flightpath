30f057eacc88abf2b2ef937fd5150da7434c20c2
Ben Griffith <bengriffith@outlook.com>
2026-01-21 19:17:17 -0700

    add argument checks and main function

--- dags/include/scripts/fetch_bts_data.py
+++ dags/include/scripts/fetch_bts_data.py
@@ -26,54 +26,71 @@ from utils.constants import (
 # default scenario for optional args
 
 
-def _minio_client():
-    client = Minio(
+# could add an end year argument that defaults to current year
+
+
+def main(start_year, end_year, month):
+    minio_client = Minio(
         endpoint="localhost:9000",
         access_key=MINIO_ROOT_USER,
         secret_key=MINIO_ROOT_PASSWORD,
         secure=False,
     )
-    return client
+
+    while start_year <= end_year:
+        for i in range(month, MAX_MONTH + 1):
+            if start_year == end_year and i == datetime.date.today().month:
+                print("breaking out of loop")
+                break
+
+            object = f"BTS/{start_year}/{BTS_FILENAME}_{start_year}_{month}.zip"
+            full_url = urljoin(BTS_BASE_URL, object)
+
+            with requests.get(full_url, stream=True) as response:
+                response.raise_for_status()
+
+                file_size = int(response.headers.get("Content-length", 0))
+
+                if file_size > 0:
+                    minio_client.put_object(
+                        bucket_name=BUCKET_BRONZE,
+                        object_name=object,
+                        data=response.raw,
+                        length=file_size,
+                        part_size=10 * 1024 * 1024,
+                        content_type=response.headers.get("Content-Type"),
+                    )
+
+            time.sleep(5)
+
+        start_year += 1
 
 
-minio_client = _minio_client()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Fetch Bureau of Transportation Statistics data"
+    )
 
-parser = argparse.ArgumentParser(
-    description="Fetch Bureau of Transportation Statistics data"
-)
-parser.add_argument("--year", type=int, default=datetime.date.today().year)
-parser.add_argument("--month", type=int, default=datetime.date.today().month)
+    parser.add_argument("--year", type=int, required=True, help="Year to start from")
+    parser.add_argument("--month", type=int, required=True, help="Month to start from")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if args.year < BTS_START_YEAR:
-    raise ValueError(f"Year must be greater than {BTS_START_YEAR}")
+    if args.year < BTS_START_YEAR:
+        raise ValueError(f"Year must be greater than {BTS_START_YEAR}")
 
-if args.month > MAX_MONTH:
-    raise ValueError(f"Month must be less than {MAX_MONTH}")
+    if args.year > datetime.date.today().year:
+        raise ValueError(
+            f"Year must be less than or equal to {datetime.date.today().year}"
+        )
 
-year = args.year
+    if args.month > MAX_MONTH:
+        raise ValueError(f"Month must be less than {MAX_MONTH}")
 
-while year <= args.year:
-    for i in range(args.month, MAX_MONTH):
-        object = f"{BTS_FILENAME}_{year}_{args.month}.zip"
-        full_url = urljoin(BTS_BASE_URL, object)
+    if (
+        args.year == datetime.date.today().year
+        and args.month > datetime.date.today().month
+    ):
+        raise ValueError("Invalid year and month combination")
 
-        with requests.get(full_url, stream=True) as response:
-            response.raise_for_status()
-
-            file_size = int(response.headers.get("Content-length", 0))
-
-            if file_size > 0:
-                minio_client.put_object(
-                    bucket_name=BUCKET_BRONZE,
-                    object_name=object,
-                    data=response.raw,
-                    length=file_size,
-                    part_size=10 * 1024 * 1024,
-                    content_type=response.headers.get("Content-Type"),
-                )
-
-        time.sleep(5)
-
-    year += 1
+    main(start_year=args.year, end_year=args.year, month=args.month)
