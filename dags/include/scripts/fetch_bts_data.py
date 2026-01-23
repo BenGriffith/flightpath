600830f362f3a4688b63b9dc5ed0417698e3e628
Ben Griffith <bengriffith@outlook.com>
2026-01-22 18:15:45 -0700

    update to use one argument

--- dags/include/scripts/fetch_bts_data.py
+++ dags/include/scripts/fetch_bts_data.py
@@ -4,6 +4,7 @@ import time
 from urllib.parse import urljoin
 
 import requests
+from dateutil.relativedelta import relativedelta
 from minio import Minio
 from utils.constants import (
     BTS_BASE_URL,
@@ -16,7 +17,32 @@ from utils.constants import (
 )
 
 
-def main(start_year, end_year):
+def _object_url(year, month):
+    object = f"BTS/{year}/{BTS_FILENAME}_{year}_{month}.zip"
+    full_url = urljoin(BTS_BASE_URL, object)
+    return object, full_url
+
+
+def _write_to_minio(minio_client, object, url):
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+
+        file_size = int(response.headers.get("Content-length", 0))
+
+        if file_size > 0:
+            minio_client.put_object(
+                bucket_name=BUCKET_BRONZE,
+                object_name=object,
+                data=response.raw,
+                length=file_size,
+                part_size=10 * 1024 * 1024,
+                content_type=response.headers.get("Content-Type"),
+            )
+
+    time.sleep(3)
+
+
+def main(end_year, month=None):
     minio_client = Minio(
         endpoint="localhost:9000",
         access_key=MINIO_ROOT_USER,
@@ -24,32 +50,19 @@ def main(start_year, end_year):
         secure=False,
     )
 
+    if month:
+        object, url = _object_url(end_year, month)
+        _write_to_minio(minio_client, object, url)
+        return "Incremental Processing Complete"
+
+    start_year = BTS_START_YEAR
     while start_year <= end_year:
-        for month in range(1, MAX_MONTH + 1):
-            if start_year == end_year and month == datetime.date.today().month:
-                print("breaking out of loop")
-
-            object = f"BTS/{start_year}/{BTS_FILENAME}_{start_year}_{month}.zip"
-            full_url = urljoin(BTS_BASE_URL, object)
-
-            with requests.get(full_url, stream=True) as response:
-                response.raise_for_status()
-
-                file_size = int(response.headers.get("Content-length", 0))
-
-                if file_size > 0:
-                    minio_client.put_object(
-                        bucket_name=BUCKET_BRONZE,
-                        object_name=object,
-                        data=response.raw,
-                        length=file_size,
-                        part_size=10 * 1024 * 1024,
-                        content_type=response.headers.get("Content-Type"),
-                    )
-
-            time.sleep(5)
+        for temp_month in range(1, MAX_MONTH + 1):
+            object, url = _object_url(end_year, temp_month)
+            _write_to_minio(minio_client, object, url)
 
         start_year += 1
+    return "Processing Complete"
 
 
 if __name__ == "__main__":
@@ -57,21 +70,14 @@ if __name__ == "__main__":
         description="Fetch Bureau of Transportation Statistics data"
     )
 
-    parser.add_argument("--start-year", type=int, required=True, help="Year from")
-    parser.add_argument(
-        "--end-year",
-        type=int,
-        required=False,
-        default=datetime.date.today().year,
-        help="Year to",
-    )
-
+    parser.add_argument("type", type=str, choices=["incremental", "all"])
     args = parser.parse_args()
 
-    if args.start_year < BTS_START_YEAR:
-        raise ValueError(f"Year must be greater than {BTS_START_YEAR}")
+    if args.type == "all":
+        end_year = datetime.date.today().year
+        main(end_year)
 
-    if args.start_year > args.end_year:
-        raise ValueError(f"Year must be less than or equal to {args.end_year}")
-
-    main(start_year=args.start_year, end_year=args.end_year)
+    if args.type == "incremental":
+        today = datetime.date.today()
+        previous_month = today - relativedelta(months=1)
+        main(end_year=previous_month.year, month=previous_month.month)
---
+++ dags/include/utils/constants.py
@@ -0,0 +1,13 @@
+import os
+
+BTS_START_YEAR = 1987
+BTS_BASE_URL = "https://transtats.bts.gov/PREZIP/"
+BTS_FILENAME = "On_Time_Reporting_Carrier_On_Time_Performance_1987_present"
+MAX_MONTH = 12
+
+BUCKET_BRONZE = "bronze"
+BUCKET_SILVER = "SILVER"
+BUCKET_GOLD = "GOLD"
+
+MINIO_ROOT_USER = os.getenv("MINIO_ROOT_USER")
+MINIO_ROOT_PASSWORD = os.getenv("MINIO_ROOT_PASSWORD")
