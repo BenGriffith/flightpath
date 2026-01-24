e3dfc588d8e4185b5b90a95a349e08ef9eb098e5
Ben Griffith <bengriffith@outlook.com>
2026-01-23 20:21:06 -0700

    remove function and correct full_url value

--- dags/include/scripts/fetch_bts_data.py
+++ dags/include/scripts/fetch_bts_data.py
@@ -4,7 +4,6 @@ import time
 from urllib.parse import urljoin
 
 import requests
-from dateutil.relativedelta import relativedelta
 from minio import Minio
 from utils.constants import (
     BTS_BASE_URL,
@@ -18,26 +17,23 @@ from utils.constants import (
 )
 
 
-def _object_url(year, month):
-    object = f"BTS/{year}/{BTS_FILENAME}_{year}_{month}.zip"
+def _write_to_minio(minio_client, year, month):
+    prefix = f"BTS/{year}"
+    object = f"{BTS_FILENAME}_{year}_{month}.zip"
     full_url = urljoin(BTS_BASE_URL, object)
-    return object, full_url
 
-
-def _write_to_minio(minio_client, object, url):
-    with requests.get(url, stream=True) as response:
+    with requests.get(full_url, stream=True) as response:
         response.raise_for_status()
-
-        file_size = int(response.headers.get("Content-length", 0))
+        breakpoint()
+        file_size = int(response.headers.get("Content-Length", 0))
 
         if file_size > 0:
             minio_client.put_object(
                 bucket_name=BUCKET_BRONZE,
-                object_name=object,
+                object_name=f"{prefix}/{object}",
                 data=response.raw,
                 length=file_size,
                 part_size=10 * 1024 * 1024,
-                content_type=response.headers.get("Content-Type"),
             )
 
     time.sleep(3)
@@ -52,16 +48,13 @@ def main(end_year, month=None):
     )
 
     if month:
-        object, url = _object_url(end_year, month)
-        _write_to_minio(minio_client, object, url)
+        _write_to_minio(minio_client, end_year, month)
         return "Incremental Processing Complete"
 
     start_year = BTS_START_YEAR
     while start_year <= end_year:
-        for temp_month in range(1, MAX_MONTH + 1):
-            object, url = _object_url(end_year, temp_month)
-            _write_to_minio(minio_client, object, url)
-
+        for _month in range(1, MAX_MONTH + 1):
+            _write_to_minio(minio_client, end_year, _month)
         start_year += 1
     return "Processing Complete"
 
@@ -71,7 +64,11 @@ if __name__ == "__main__":
         description="Fetch Bureau of Transportation Statistics data"
     )
 
-    parser.add_argument("type", type=str, choices=["incremental", "all"])
+    parser.add_argument(
+        "--type", type=str, required=True, choices=["incremental", "all"]
+    )
+    parser.add_argument("--year", type=int)
+    parser.add_argument("--month", type=int)
     args = parser.parse_args()
 
     if args.type == "all":
@@ -79,6 +76,21 @@ if __name__ == "__main__":
         main(end_year)
 
     if args.type == "incremental":
-        today = datetime.date.today()
-        previous_month = today - relativedelta(months=1)
-        main(end_year=previous_month.year, month=previous_month.month)
+        if args.year is None or args.month is None:
+            parser.error("argument --type incremental requires --year and --month")
+
+        if args.year < BTS_START_YEAR:
+            parser.error(f"argument --year must be greater than {BTS_START_YEAR}")
+
+        year = datetime.date.today().year
+        month = datetime.date.today().month
+        if args.year > year:
+            parser.error(f"argument --year must be less than {year}")
+
+        if args.year == year and args.month >= month:
+            parser.error(f"argument --month must be less than {month}")
+
+        if args.month > MAX_MONTH:
+            parser.error(f"argument --month must be less than {MAX_MONTH}")
+
+        main(args.year, args.month)
